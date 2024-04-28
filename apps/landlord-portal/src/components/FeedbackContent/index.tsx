248107cd558c85459564ff042e54e2c1b1358ee0
ife <adeekofeyisetan@yahoo.com>
2024-04-28 06:17:24 +0100

    CU-86bygtcet - Email Verification - UI

--- apps/landlord-portal/src/components/FeedbackContent/index.tsx
+++ apps/landlord-portal/src/components/FeedbackContent/index.tsx
@@ -1,40 +1,68 @@
-import { Stack, Typography, useTheme,Button } from "@mui/material";
-import { MdErrorOutline } from "react-icons/md";
-import { BsCheck2Circle } from "react-icons/bs";
-import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
+import { Stack, Typography, Button } from "@mui/material";
+import Logo1 from "../../assets/images/circle-ok.svg";
+import Logo2 from "../../assets/images/error.svg";
+
 interface FeedbackProps {
   content: string;
   type: "success" | "error";
-  // onDismiss: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
-const FeedbackContent = ({  content, type }: FeedbackProps) => {
-  const theme = useTheme();
-
+const FeedbackContent = ({ content, type,onClick }: FeedbackProps) => {
   return (
     <Stack alignItems={"center"} justifyContent="center" spacing={1}>
       {type === "success" ? (
-      <> <CheckCircleOutlineOutlinedIcon sx={{ fontSize: "60px" }} color="success" />
-        <Typography variant="h6" align="center">
-        { "Email Verified "}
-      </Typography>
-      <Typography variant="subtitle1"  align="center">
-        {content || "Verify Your "}
-      </Typography>
-      <Button>Sign in</Button></> 
+        <>
+          {" "}
+          <div style={{ marginBottom: "32px" }}>
+            <img src={Logo1} alt="logo" width={"100px"} height={"100px"} />
+          </div>
+          <div style={{ marginBottom: "20px" }}>
+            <Typography align="center" fontSize={"30px"} fontWeight={700}>
+              {"Email Verified "}
+            </Typography>{" "}
+          </div>
+          <div style={{ marginBottom: "30px" }}>
+            <Typography fontSize={"18px"} fontWeight={500} textAlign={"left"}>
+              {content || "Verify Your "}
+            </Typography>
+          </div>
+          <Button
+            onClick={onClick}
+            sx={{
+              border: "1px solid #002147",
+              color: "white",
+              background: "#002147",
+              height: "40px",
+              width: "90px",
+              borderRadius: "8px",
+              "&:hover": {
+                color: "#002147",
+                background: "#FFFFFF",
+                cursor: "pointer",
+              },
+            }}
+          >
+            Sign In
+          </Button>
+        </>
       ) : type === "error" ? (
-        <> <MdErrorOutline size={60} color={theme.palette.error.light} />
-        <Typography variant="h6" align="center">
-        { "Verify Your Email "}
-      </Typography>
-      <Typography variant="subtitle1"  align="center">
-        {content || "Verify Your "}
-      </Typography></>
+        <>
+          {" "}
+          <div style={{ marginBottom: "32px" }}>
+            <img src={Logo2} alt="logo" width={"100px"} height={"100px"} />
+          </div>
+          <div style={{ marginBottom: "30px" }}>
+            <Typography align="center" fontSize={"30px"} fontWeight={700}>
+              {"Verify Your Email "}
+            </Typography>
+          </div>
+          <div>
+            <Typography fontSize={"18px"} fontWeight={500} textAlign={"left"}>
+              {content}
+            </Typography>
+          </div>
+        </>
       ) : null}
-
-     
-      {/* <Button onClick={onDismiss} variant="contained" color="secondary">
-        Okay
-      </Button> */}
     </Stack>
   );
 };
--- apps/landlord-portal/src/pages/EmailVerification/index.tsx
+++ apps/landlord-portal/src/pages/EmailVerification/index.tsx
@@ -1,40 +1,38 @@
 import { FC } from "react";
-import { useState, useEffect } from "react";
-import { Box, Grid, Container,Typography, Paper } from "@mui/material";
-import Logo from "../../assets/images/Group 1000002043.png";
+import { useState } from "react";
+import { Box, Grid } from "@mui/material";
 import FeedbackContent from "../../components/FeedbackContent";
-import { useNavigate } from "react-router-dom";
 
 interface EmailVerificationProps {}
 
 const EmailVerification: FC<EmailVerificationProps> = ({}) => {
   const [verifySuccess, setVerifySuccess] = useState(true);
   const [error, setError] = useState(null);
-  const navigate = useNavigate();
-  const backToHome = () => navigate("/", { replace: true });
 
   const renderViewContent = () => {
     if (verifySuccess)
       return (
         <FeedbackContent
-          content={"Your Email Address has been verified,You can continue using the application."}
-          // onDismiss={backToHome}
+          content={
+            "Your Email Address has been verified,You can continue using the application."
+          }
+          onClick={()=>console.log("here")}
           type="success"
         />
       );
-    if (error) return <FeedbackContent content={error} type="error" />;
-    else return <><Typography variant="h4" >Invalid Link Or Page is broken</Typography></>;
+    if (error)
+      return (
+        <FeedbackContent
+          content="A verification link has been sent to your email address. Please check your email and click on the link to continue"
+          type="error"
+          onClick={()=>console.log("here")}
+
+        />
+      );
+    else return null;
   };
   return (
     <Grid container spacing={1} bgcolor="white">
-      <Grid item xs={12} sm={12} md={12} lg={12}>
-        <img
-          src={Logo}
-          alt="companylogo"
-          style={{ width: "159px", height: "32px", margin: "1rem" }}
-          onClick={backToHome}
-        />
-      </Grid>
       <Grid
         item
         xs={12}
@@ -45,25 +43,28 @@ const EmailVerification: FC<EmailVerificationProps> = ({}) => {
           textAlign: "center",
           display: "flex",
           justifyContent: "center",
-          marginTop: "15rem",
+          alignSelf: "center",
         }}
       >
-        <Paper
-        elevation={2}
-        square
-        sx={{
-          minHeight: 280,
-          px: 8,
-          py: 6,
-          minWidth: 485,
-          maxWidth: 725,
-          alignSelf: "center",
-          position: "relative",
-        }}>
-        {renderViewContent()}
-        </Paper>
-       
+        <Box
+          sx={{
+            // minHeight: 280,
+            px: 8,
+            py: 6,
+            minWidth: 485,
+            maxWidth: 725,
+            alignSelf: "center",
+            marginTop: "205px",
+          }}
+        >
+          {/* <FeedbackContent
+            content="A verification link has been sent to your email address. Please check your email and click on the link to continue"
+            type="error"
+            onClick={()=>console.log("here")}
 
+          /> */}
+          {renderViewContent()}
+        </Box>
       </Grid>
     </Grid>
   );
