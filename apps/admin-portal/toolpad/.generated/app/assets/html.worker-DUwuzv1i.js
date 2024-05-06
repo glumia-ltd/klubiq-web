1d3a738d43a74753123cd6caf2c96d185d7daa74
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-05-06 15:58:08 -0400

    Add generated files for toolpad app

---
+++ apps/admin-portal/toolpad/.generated/app/assets/html.worker-DUwuzv1i.js
@@ -0,0 +1,1 @@
+function e(r){return new Worker("/prod/assets/html.worker-BJMlcbMU.js",{name:r==null?void 0:r.name})}export{e as default};
---
+++ apps/admin-portal/toolpad/.generated/app/assets/json.worker-C-2lW7b8.js
@@ -0,0 +1,1 @@
+function e(r){return new Worker("/prod/assets/json.worker-BwvX8PuZ.js",{name:r==null?void 0:r.name})}export{e as default};
