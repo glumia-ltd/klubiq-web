54330e59ea23e8cb77be5cdb1c6b1449db5a3dc0
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-05-07 01:20:53 -0400

    chore: Update base URL for API requests in landlord-portal

--- apps/landlord-portal/src/api/index.ts
+++ apps/landlord-portal/src/api/index.ts
@@ -4,7 +4,7 @@ import store from '../store';
 
 const api = axios.create({
   //TODO add base URL
-  baseURL: import.meta.env.NODE_ENV !== 'development' ? `${import.meta.env.VITE_BASE_URL_DEV}/api` : '/api',
+  baseURL: import.meta.env.NODE_ENV !== 'local' ? `${import.meta.env.VITE_BASE_URL_DEV}/api` : '/api',
 });
 
 // request config
