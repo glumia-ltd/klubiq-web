6e5891e3a4de96b3095fd76a98e4d281e443400b
OluwadahunsiA <tommydate0@gmail.com>
2024-06-02 23:26:44 +0300

    edit user

--- apps/landlord-portal/src/helpers/endpoints.ts
+++ apps/landlord-portal/src/helpers/endpoints.ts
@@ -3,6 +3,6 @@ export const authEndpoints = {
 	signup: () => 'auth/landlord-signup',
 	emailVerification: () => 'auth/email-verification-link',
 	refreshToken: () => 'auth/exchange-refresh-token',
-	getUserByFbid: (uid: string) => `auth/user/${uid}`,
+	getUserByFbid: () => `auth/user`,
 	resetPassword: () => `auth/reset-password-link`,
 };
