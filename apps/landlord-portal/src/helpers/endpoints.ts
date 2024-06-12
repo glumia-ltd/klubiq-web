7db99f09ded55aac382cf2a4206d10c77c525a39
OluwadahunsiA <tommydate0@gmail.com>
2024-06-12 19:34:37 +0300

    fix endpoint

--- apps/landlord-portal/src/helpers/endpoints.ts
+++ apps/landlord-portal/src/helpers/endpoints.ts
@@ -1,6 +1,6 @@
 export const authEndpoints = {
 	login: () => 'auth/login',
-	signup: () => 'auth/landlord-signup',
+	signup: () => 'auth/landlord/signup',
 	emailVerification: () => 'auth/email-verification-link',
 	refreshToken: () => 'auth/exchange-refresh-token',
 	getUserByFbid: () => `auth/user`,
