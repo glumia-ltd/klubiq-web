5e7cc73ce567df6535126b04f71b7781e1f8dc5a
ifeoluwa2652 <feyi.durojaiye@glumia.ng>
2025-04-29 16:10:21 +0100

    setting integration cleanup

--- src/pages/AddTenantPage/AddTenantPage.tsx
+++ src/pages/AddTenantPage/AddTenantPage.tsx
@@ -3,29 +3,19 @@ import AddPropertiesInformationLayout from '../../Layouts/AddPropertiesInformati
 import { useLocation } from 'react-router-dom';
 import { PropertyDataType } from '../../shared/type';
 
-// const useQuery = () => {
-// 	return new URLSearchParams(useLocation().search);
-// };
+
 
 interface LocationState {
-    currentProperty?: PropertyDataType // Define the type of currentProperty
+	currentProperty?: PropertyDataType; 
 }
 const AddTenantPage = () => {
 	const location = useLocation() as { state: LocationState };
-	const { currentProperty } = location.state || {}; // Destructure currentProperty from state
-	// if (!currentProperty) {
-	// 	// Handle the case where currentProperty is not available
-	// 	return <div>Error: Property not found.</div>;
-	// }
-	// const query = useQuery();
-	// const propertyId = query.get('property');
-	// const unitId = query.get('unit');
+	const { currentProperty } = location.state || {}; 
+
 	return (
 		<AddPropertiesInformationLayout>
 			{currentProperty ? (
-								<AddTenant propertyDetails={currentProperty} />
-
-				// <AddTenant propertyDetails={currentProperty} propertyId={propertyId} unitId={unitId} />
+				<AddTenant propertyDetails={currentProperty} />
 			) : (
 				<div>Error: Property details are missing.</div>
 			)}
