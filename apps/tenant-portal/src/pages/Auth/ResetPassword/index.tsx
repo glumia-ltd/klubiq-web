a18a68baa3b8f261660fc1899917bc9ae7b11d8e
Bolu Johnson <j.boluwatife@klubiq.com>
2025-05-16 05:18:02 +0100

    feat: Implement reset password functionality and add Snackbar for notifications

--- apps/tenant-portal/src/pages/Auth/ResetPassword/index.tsx
+++ apps/tenant-portal/src/pages/Auth/ResetPassword/index.tsx
@@ -1,10 +1,16 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import * as Yup from 'yup';
-import { useNavigate } from 'react-router-dom';
-import { Grid, Typography } from '@mui/material';
+
 import { FormField, FormGroup, KlubiqForm } from '@klubiq/ui-components';
+import { Grid, Typography } from '@mui/material';
+
+import { openSnackbar } from '@/store/SnackbarStore/SnackbarSlice';
+import { authEndpoints } from '@/helpers/endpoints';
+import { api } from "@/api"
 
 import Logo from '@/assets/images/icons.svg';
-
 import { styles } from '../styles';
 
 type IPasswordType = {
@@ -14,15 +20,19 @@ type IPasswordType = {
 
 const ResetPassword = () => {
 	const navigate = useNavigate();
+	const [searchParams] = useSearchParams();
+	const dispatch = useDispatch()
+	
 
-	// Password Regex
-	//   Has minimum 8 characters in length. Adjust it by modifying {8,}
-	// At least one uppercase English letter. You can remove this condition by removing (?=.*?[A-Z])
-	// At least one lowercase English letter.  You can remove this condition by removing (?=.*?[a-z])
-	// At least one digit. You can remove this condition by removing (?=.*?[0-9])
-	// At least one special character,  You can remove this condition by removing (?=.*?[#?!@$%^&*-])
+	const [loading, setLoading] = useState<boolean>(false)
 
 	const resetPasswordFormFields: (FormField | FormGroup)[] = [
+		// Password Regex
+		//   Has minimum 8 characters in length. Adjust it by modifying {8,}
+		// At least one uppercase English letter. You can remove this condition by removing (?=.*?[A-Z])
+		// At least one lowercase English letter.  You can remove this condition by removing (?=.*?[a-z])
+		// At least one digit. You can remove this condition by removing (?=.*?[0-9])
+		// At least one special character,  You can remove this condition by removing (?=.*?[#?!@$%^&*-])
 		{
 			name: 'password',
 			label: 'Password',
@@ -64,8 +74,32 @@ const ResetPassword = () => {
 	};
 
 	const onSubmit = async (values: IPasswordType) => {
-		console.log(values);
-		navigate('/');
+		setLoading(true)
+
+		const payload = {
+			obbCode: searchParams.get('oobCode'),
+			email: searchParams.get('email')!.replace(' ', '+'),
+			password: values.password
+		};
+
+		try {
+			const response = await api.post(
+				authEndpoints.acceptInvitation(searchParams.get('token')as string),
+				payload,
+			);
+
+			console.log(response)
+			navigate('/');
+		} catch (e) {
+			console.error(e)
+			dispatch(
+				openSnackbar({
+					message: 'An error occurred',
+					severity: 'error',
+					isOpen: true
+				})
+			)
+		}
 	};
 
 	return (
