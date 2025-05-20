f0ba52c4c8f7abb738d1b35371606744c6d8b09c
Vincent Adeyemi <vinadeyems@gmail.com>
2025-05-20 13:57:10 -0400

    refactor: Comment out unused imports and components in PrivateRoute for cleaner code
    - Commented out unused imports including useAuth, MFAPrompt, SessionTimeoutProvider, and AlertDialog.
    - Simplified the PrivateRoute component by removing MFA and alert dialog logic, focusing on the Outlet rendering.

--- apps/landlord-portal/src/router/PrivateRoute.tsx
+++ apps/landlord-portal/src/router/PrivateRoute.tsx
@@ -1,21 +1,21 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getAuthState } from '../store/AuthStore/AuthSlice';
-import useAuth from '../hooks/useAuth';
-import MFAPrompt from '../components/Dialogs/MfaPrompts';
-import { SessionTimeoutProvider } from '../context/SessionContext/SessionTimeoutContext';
-import AlertDialog from '../components/Dialogs/AlertDialog';
+// import useAuth from '../hooks/useAuth';
+// import MFAPrompt from '../components/Dialogs/MfaPrompts';
+// import { SessionTimeoutProvider } from '../context/SessionContext/SessionTimeoutContext';
+// import AlertDialog from '../components/Dialogs/AlertDialog';
 import { consoleDebug } from '../helpers/debug-logger';
 
 const PrivateRoute = () => {
 	const {isSignedIn } = useSelector(getAuthState);
-	const {
-		showMFAPrompt,
-		goToMFASetup,
-		optOutOf2fa,
-		handleCloseMFAPrompt,
-		alertDialogs,
-	} = useAuth();
+	// const {
+	// 	showMFAPrompt,
+	// 	goToMFASetup,
+	// 	optOutOf2fa,
+	// 	handleCloseMFAPrompt,
+	// 	alertDialogs,
+	// } = useAuth();
 
 
 	const loginUrl = 'login';
@@ -25,34 +25,35 @@ const PrivateRoute = () => {
 	}
 
 	return (
-		<SessionTimeoutProvider>
-			{showMFAPrompt && (
-				<MFAPrompt
-					open={showMFAPrompt}
-					onClose={() => {
-						handleCloseMFAPrompt();
-					}}
-					onMFASetupClick={goToMFASetup}
-					onOptOutClick={optOutOf2fa}
-				></MFAPrompt>
-			)}
-			{alertDialogs.length > 0 &&
-				alertDialogs.map((alert) => (
-					<AlertDialog
-						key={alert.id}
-						open={alert.open}
-						title={alert.title}
-						message={alert.message}
-						onClose={alert.onClose}
-						onConfirmClick={alert.onConfirmClick}
-						onCancelClick={alert.onCancelClick}
-						id={alert.id}
-						cancelButtonText={alert.cancelButtonText}
-						confirmButtonText={alert.confirmButtonText}
-					></AlertDialog>
-				))}
-			<Outlet />
-		</SessionTimeoutProvider>
+		<Outlet />
+		// <SessionTimeoutProvider>
+		// 	{showMFAPrompt && (
+		// 		<MFAPrompt
+		// 			open={showMFAPrompt}
+		// 			onClose={() => {
+		// 				handleCloseMFAPrompt();
+		// 			}}
+		// 			onMFASetupClick={goToMFASetup}
+		// 			onOptOutClick={optOutOf2fa}
+		// 		></MFAPrompt>
+		// 	)}
+		// 	{alertDialogs.length > 0 &&
+		// 		alertDialogs.map((alert) => (
+		// 			<AlertDialog
+		// 				key={alert.id}
+		// 				open={alert.open}
+		// 				title={alert.title}
+		// 				message={alert.message}
+		// 				onClose={alert.onClose}
+		// 				onConfirmClick={alert.onConfirmClick}
+		// 				onCancelClick={alert.onCancelClick}
+		// 				id={alert.id}
+		// 				cancelButtonText={alert.cancelButtonText}
+		// 				confirmButtonText={alert.confirmButtonText}
+		// 			></AlertDialog>
+		// 		))}
+		// 	<Outlet />
+		// </SessionTimeoutProvider>
 	);
 };
 
