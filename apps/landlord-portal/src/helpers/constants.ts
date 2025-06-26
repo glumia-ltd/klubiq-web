5cd04f051ce39763c4c6f457cfd82ffbfd487a27
Vincent Adeyemi <vinadeyems@gmail.com>
2025-06-26 17:09:30 -0400

    feat: Enhance TenantDialog and AddTenantForm for improved tenant management
    - Introduced a close button in TenantDialog for better user experience when selecting tenants.
    - Updated AddTenantForm to handle tenant data submission more efficiently, including conditional navigation based on return path.
    - Refactored API call logic in AddTenantForm for clarity and consistency.
    - Added dynamic tenant selection functionality in LeaseDetails, allowing for better tenant management within leases.

--- apps/landlord-portal/src/helpers/constants.ts
+++ apps/landlord-portal/src/helpers/constants.ts
@@ -1,14 +1,20 @@
-import { SimpleTableColumn } from "../page-tytpes/properties/detail-page.types";
+import { SimpleTableColumn } from '../page-tytpes/properties/detail-page.types';
 export const ALL_TABS = ['Overview', 'Lease', 'Document'];
 export const PROPERTY_CONSTANTS = {
-    tabs: ALL_TABS,
-    leaseTableColumns: [
-      { id: '1', label: 'Tenant' },
-      { id: '2', label: 'Status' },
-      { id: '3', label: 'Rent Amount' },
-      { id: '4', label: 'Start Date' },
-      { id: '5', label: 'End Date' },
-    ] as SimpleTableColumn[]
-  } as const;
+	tabs: ALL_TABS,
+	leaseTableColumns: [
+		{ id: '1', label: 'Tenant' },
+		{ id: '2', label: 'Status' },
+		{ id: '3', label: 'Rent Amount' },
+		{ id: '4', label: 'Start Date' },
+		{ id: '5', label: 'End Date' },
+	] as SimpleTableColumn[],
+} as const;
 
-  export const PERSON_TITLES = ['Mr', 'Mrs', 'Ms', 'Dr', 'Chief'] as const;
+export const PERSON_TITLES = ['Mr', 'Mrs', 'Ms', 'Dr', 'Chief'] as const;
+export const defaultOrgSettings = {
+	currency: 'NGN',
+	language: 'en',
+	countryCode: 'NG',
+	currencySymbol: '₦',
+} as const;
--- apps/landlord-portal/src/pages/Features/Lease/LeaseDetails.tsx
+++ apps/landlord-portal/src/pages/Features/Lease/LeaseDetails.tsx
@@ -16,7 +16,7 @@ import LeasePropertyCard from '../../../components/LeaseCards/LeasePropertyCard'
 import MiniCard from '../../../components/LeaseCards/MiniCard';
 // import DocumentUploadCard from '../../components/LeaseCards/DocumentUploadCard';
 import { LeaseDocumentTable } from './LeaseDocumentTable';
-import { useGetSingleLeaseByIdQuery } from '../../../store/LeaseStore/leaseApiSlice';
+import { useGetOrgPropertiesViewListQuery, useGetSingleLeaseByIdQuery } from '../../../store/LeaseStore/leaseApiSlice';
 import { useLocation, useNavigate } from 'react-router-dom';
 import {
 	DateStyle,
@@ -31,18 +31,94 @@ import ViewListOutlinedIcon from '@mui/icons-material/ViewListOutlined';
 import { useDynamicBreadcrumbs } from '../../../hooks/useDynamicBreadcrumbs';
 import { Breadcrumb } from '../../../components/Breadcrumb';
 import { statusColors } from '../../../page-tytpes/leases/list-page.type';
+import { TenantDialog } from '../../../components/CustomFormComponents/TenantDialog';
+import { DynamicTanstackFormProps, KlubiqFormV1, DynamicModal, DynamicModalProps } from '@klubiq/ui-components';
 
+
+function renderTenantSelectField(fieldApi: any, fieldConfig: any, form: any) {
+	return (
+		<TenantDialog
+			field={{
+				fieldConfig: {
+					...fieldConfig,
+					options: Array.isArray(fieldConfig.options)
+						? fieldConfig.options.map(
+								(opt: {
+									value: string | number;
+									label: string;
+									email: string;
+								}) => ({
+									...opt,
+									value: String(opt.value),
+									label: `${opt.label}`,
+								}),
+							)
+						: typeof fieldConfig.options === 'function'
+							? fieldConfig
+									.options(form.getValues())
+									.map(
+										(opt: {
+											value: string | number;
+											label: string;
+											email: string;
+										}) => ({
+											...opt,
+											value: String(opt.value),
+											label: `${opt.label}`,
+										}),
+									)
+							: [],
+				},
+				state: fieldApi.state,
+				handleChange: fieldApi.handleChange,
+			}}
+			form={form}
+		/>
+	);
+}
 const LeaseDetails = () => {
 	const location = useLocation();
 	const navigate = useNavigate();
 	const { user } = useSelector(getAuthState);
 	const [open, setOpen] = useState<boolean>(false);
-
+	const [openAddTenants, setOpenAddTenants] = useState<boolean>(false);
 	const { updateBreadcrumb } = useDynamicBreadcrumbs();
 	const timeDateOptions = {
 		dateStyle: DateStyle.FULL,
 		hour12: true,
 	};
+	const initialValues = {
+		tenantsIds: [],
+		primaryTenantId: '',
+	}
+
+	const modalConfig  =  (header: string): DynamicModalProps => {
+		return {
+			open: openAddTenants,
+			onClose: () => setOpenAddTenants(false),
+			headerText: header,
+			headerAlign: 'center',
+			contentAlign: 'center',
+			contentDirection: 'column',
+			borderRadius: 1,
+			maxWidth: 'sm',
+			fullScreenOnMobile: true,
+			children: <KlubiqFormV1 {...addTenantsFormConfig} />,
+			sx: {
+				height: 'auto',
+			},
+		} as DynamicModalProps;
+	}
+	
+	const tenantsQueryResult = useGetOrgPropertiesViewListQuery(
+		{ orgId: user?.organizationUuid },
+		{
+			selectFromResult: ({ data, isLoading: loading }) => ({
+				tenants: data?.tenants ? [...data.tenants] : [],
+				isLoading: loading,
+			}),
+		},
+	);
 	const currentLeaseId = location.pathname.split('/')[2]!;
 	const handleToggle = () => setOpen((prevOpen) => !prevOpen);
 	const anchorRef = useRef<HTMLButtonElement>(null);
@@ -58,6 +134,61 @@ const LeaseDetails = () => {
 		],
 		row: [{ name: 'Maintenance Fee', date: 'second' }],
 	};
+ 
+	const onAddTenantsSubmit = async (values: any) => {
+		console.log(values);
+	}
+	const getSelectedTenants = (values: any) => {
+		const selectedTenants = tenantsQueryResult.tenants.filter((tenant: { id: string; }) => values?.tenantsIds?.includes(tenant.id)) || []; 
+		return selectedTenants.map((tenant: { id: string; firstName: string; lastName: string; email: string; companyName?: string; }) => ({
+			label: `${tenant.companyName || ''} ${tenant.companyName && (tenant.firstName || tenant.lastName) ? ' - ' : ''}${tenant.firstName} ${tenant.lastName}`,
+			value: tenant.id,
+		}));
+	}
+	const addTenantsFormConfig: DynamicTanstackFormProps = {
+		initialValues: initialValues,
+		fields: [
+			{
+				name: 'tenantsIds',
+				label: 'Select Tenants',
+				type: 'checkbox-group',
+				options: tenantsQueryResult.tenants.map(
+					(tenant: {
+						firstName: string;
+						lastName: string;
+						email: string;
+						id: string;
+						companyName?: string;
+					}) => ({
+						label: `${tenant.companyName || ''} ${tenant.companyName && (tenant.firstName || tenant.lastName) ? ' - ' : ''}${tenant.firstName} ${tenant.lastName}`,
+						value: tenant.id,
+						email: tenant.email,
+					}),
+				),
+				customComponent: renderTenantSelectField,
+			},
+			{
+				name: 'primaryTenantId',
+				label: 'Select Primary Tenant',
+				type: 'select',
+				options: (values: any) => {
+					return getSelectedTenants(values);
+				},
+			},
+			
+		],
+		onSubmit: onAddTenantsSubmit,
+		submitButtonText: 'Add Tenants',
+		showTopBackButton: false,
+		showBackdrop: true,
+		backdropText: 'Please wait while we add tenants...',
+		verticalAlignment: 'top',
+		horizontalAlignment: 'center',
+	}
+
+	const handleAddTenants = () => {
+		setOpenAddTenants(true);
+	}
 
 	useEffect(() => {
 		const newBreadcrumbs: Record<string, BreadcrumbItem> = {
@@ -139,13 +270,20 @@ const LeaseDetails = () => {
 												aria-labelledby='composition-button'
 												// onKeyDown={handleListKeyDown}
 											>
-												<MenuItem
+												{ leaseData?.status !== 'Terminated' && leaseData?.status !== 'Expired' && leaseData?.status !== 'Archived' && <MenuItem
+													onClick={handleAddTenants}
+													sx={{ padding: '10px' }}
+													divider
+												>
+													Add Tenant(s)
+												</MenuItem>}
+												{ !leaseData?.isArchived && <MenuItem
 													// onClick={handleArchiveLease}
 													sx={{ padding: '10px' }}
 													divider
 												>
 													Archive Lease
-												</MenuItem>
+												</MenuItem>}
 												<MenuItem
 													// onClick={handleEditLease}
 													sx={{ padding: '10px' }}
@@ -153,12 +291,12 @@ const LeaseDetails = () => {
 												>
 													Edit Lease
 												</MenuItem>
-												<MenuItem
+												{ leaseData?.status !== 'Terminated' && <MenuItem
 													// onClick={handleTerminateLease}
 													sx={{ padding: '10px' }}
 												>
 													Terminate Lease
-												</MenuItem>
+												</MenuItem>}
 											</MenuList>
 										</ClickAwayListener>
 									</Paper>
@@ -254,6 +392,7 @@ const LeaseDetails = () => {
 					<LeaseDocumentTable documentTableData={documentTableData} />
 				</Stack>
 			</Stack>
+			<DynamicModal {...modalConfig('Add Tenants')} />
 		</>
 	);
 };
